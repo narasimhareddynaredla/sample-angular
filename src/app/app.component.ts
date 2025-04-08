229cd37cd6069e53c93b45be1614913425bd7a54
Narasimha Reddy Naredla <102274889+narasimhareddynaredla@users.noreply.github.com>
2025-04-08 15:44:56 +0530

    Update app.component.ts
    angular header changing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-sample-app';
+  title = 'jenkins-angular-app-deployment';
 }
